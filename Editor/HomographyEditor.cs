cb96097ed8a93a584834751b512ebd594c93abdb
hecomi <info@hecomi.com>
2017-09-16 15:54:21 +0900

    add editor script.

---
+++ Editor/HomographyEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace uHomography
+{
+
+[CustomEditor(typeof(Homography))]
+public class HomographyEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    }
+}
+
+}
